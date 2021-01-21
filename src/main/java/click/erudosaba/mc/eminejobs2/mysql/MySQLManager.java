7bbb657fb5a555291c8bce11276c79675918a180
Koryu25 <komu.ryu@icloud.com>
2021-01-21 21:48:57 +0900

    [add] MySQLに関するクラスの追加

--- src/main/java/click/erudosaba/mc/eminejobs2/mysql/MySQLManager.java
+++ src/main/java/click/erudosaba/mc/eminejobs2/mysql/MySQLManager.java
@@ -1,4 +1,6 @@
-package click.erudosaba.mc.eminejobs2.database;
+package click.erudosaba.mc.eminejobs2.mysql;
+
+import org.bukkit.Bukkit;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,6 +9,7 @@ import java.sql.SQLException;
 public class MySQLManager {
 
     private Connection connection;
+
     private final String host;
     private final int port;
     private final String database;
@@ -19,6 +22,8 @@ public class MySQLManager {
         this.username = username;
         this.password = password;
         this.port = port;
+
+        connectionTest();
     }
 
     public Boolean connectionTest() {
@@ -31,12 +36,15 @@ public class MySQLManager {
         }
     }
     private void openConnection() throws SQLException, ClassNotFoundException {
-        if (connection != null && !connection.isClosed()) return;
         synchronized (this) {
             if (connection != null && !connection.isClosed()) return;
             Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection("jdbc:mysql://"+ host +":"+ port +"/"+ database, username, password);
+            //Message
+            Bukkit.getLogger().info("§3Setting up MySQL");
         }
     }
-
+    public Connection getConnection() {
+        return connection;
+    }
 }
