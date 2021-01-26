0ded055b14922388ef42ffbd89a661f3a198d857
erudo <hashierudo@gmail.com>
2021-01-26 15:56:24 +0900

    [add] CustomConfig.ktの追加
    CustomConfigクラスの実装

--- src/main/kotlin/click/erudosaba/mc/eminejobs2/jobs/JobManager.kt
+++ src/main/kotlin/click/erudosaba/mc/eminejobs2/jobs/JobManager.kt
@@ -1,69 +1,26 @@
 package click.erudosaba.mc.eminejobs2.jobs
 
 import click.erudosaba.mc.eminejobs2.Main
+import click.erudosaba.mc.eminejobs2.util.CustomConfig
 import org.bukkit.configuration.InvalidConfigurationException
 import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.craftbukkit.libs.org.apache.commons.io.FileUtils
 import java.io.File
 import java.io.IOException
 
-class JobManager(private var plugin: Main, private val fileName: String) {
+class JobManager(var plg: Main, private val fileName: String) : CustomConfig(plugin=plg, path="jobs/${fileName}") {
 
-    private val configFile = File(plugin.dataFolder, "/jobs/$fileName")
-    private val config = YamlConfiguration()
-
-    val id : Int
-    val JobName : String?
-    val category : JobCategory
-    val JobExp : Double
-    val items : List<String>
-    val skills : List<String>
-
-    init {
-        this.saveDefault()
-        this.reload()
-
-        id = config.getInt("ID")
-        JobName = config.getString("JobName")
-        category =
-                try {
-                    JobCategory.valueOf(config.getString("Category").toString())
-                } catch(e: IllegalArgumentException) {
-                    e.printStackTrace()
-                    JobCategory.INVALID
-                }
-        JobExp = config.getDouble("JobExp")
-        items = config.getStringList("Items")
-        skills = config.getStringList("Skills")
-
-    }
-
-    private fun reload() {
-        try {
-            config.load(configFile)
-        } catch(e: IOException) {
-            e.printStackTrace()
-        } catch(e: InvalidConfigurationException) {
-            e.printStackTrace()
-        }
-
-        val defaultConfigStream = plugin.getResource("/jobs/$fileName")
-        if(defaultConfigStream != null) {
-            val file = File(plugin.dataFolder, "/jobs/$fileName")
+    val id : Int = config.getInt("ID")
+    val JobName : String? = config.getString("JobName")
+    val category : JobCategory =
             try {
-                FileUtils.copyInputStreamToFile(defaultConfigStream,file)
-            } catch(e: IOException) {
+                JobCategory.valueOf(config.getString("Category").toString())
+            } catch(e: IllegalArgumentException) {
                 e.printStackTrace()
+                JobCategory.INVALID
             }
+    val JobExp : Double = config.getDouble("JobExp")
+    val items : List<String> = config.getStringList("Items")
+    val skills : List<String> = config.getStringList("Skills")
 
-            val defaultConfig = YamlConfiguration.loadConfiguration(file)
-            config.setDefaults(defaultConfig)
-        }
-    }
-
-    private fun saveDefault() {
-        if(!configFile.exists()) {
-            plugin.saveResource("jobs/$fileName",false)
-        }
-    }
 }
---
+++ src/main/kotlin/click/erudosaba/mc/eminejobs2/util/MyConfig.kt
@@ -0,0 +1,7 @@
+package click.erudosaba.mc.eminejobs2.util
+
+import click.erudosaba.mc.eminejobs2.Main
+
+class MyConfig(val plg: Main) : CustomConfig(plugin=plg,path = null) {
+
+}
