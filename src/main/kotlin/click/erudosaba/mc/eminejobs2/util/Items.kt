bc14053aa39a3eff13fac94673350129009dcc90
erudo <hashierudo@gmail.com>
2021-01-28 19:24:42 +0900

    [update] add axes and shovels
    斧一覧とショベル一覧の追加

--- src/main/kotlin/click/erudosaba/mc/eminejobs2/util/Items.kt
+++ src/main/kotlin/click/erudosaba/mc/eminejobs2/util/Items.kt
@@ -1,9 +1,32 @@
 package click.erudosaba.mc.eminejobs2.util
 
-import org.bukkit.Material
+import org.bukkit.Material.*
 
 object Items {
     val pickaxes = arrayOf(
-            Material.WOODEN_PICKAXE
+            WOODEN_PICKAXE,
+            STONE_PICKAXE,
+            IRON_PICKAXE,
+            GOLDEN_PICKAXE,
+            DIAMOND_PICKAXE,
+            NETHERITE_PICKAXE
+    )
+
+    val axes = arrayOf(
+            WOODEN_AXE,
+            STONE_AXE,
+            IRON_AXE,
+            GOLDEN_AXE,
+            DIAMOND_AXE,
+            NETHERITE_AXE
+    )
+
+    val shovels = arrayOf(
+            WOODEN_SHOVEL,
+            STONE_SHOVEL,
+            IRON_SHOVEL,
+            GOLDEN_SHOVEL,
+            DIAMOND_SHOVEL,
+            NETHERITE_SHOVEL
     )
 }
