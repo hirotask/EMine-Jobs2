2de8d5fabdecaa0fdfec65bcac72187e2cc3ccd6
erudo <hashierudo@gmail.com>
2021-01-28 21:22:18 +0900

    [add] add SmeltEvent
    SmeltEventの追加（まだ未完成）

---
+++ src/main/kotlin/click/erudosaba/mc/eminejobs2/event/SmeltEvent.kt
@@ -0,0 +1,33 @@
+package click.erudosaba.mc.eminejobs2.event
+
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.event.Cancellable
+import org.bukkit.event.Event
+import org.bukkit.event.HandlerList
+import org.bukkit.inventory.ItemStack
+
+class SmeltEvent(val player : Player, val item : ItemStack) : Event(), Cancellable{
+    var cancel = false
+
+    override fun getHandlers(): HandlerList {
+        return HANDLERS
+    }
+
+    override fun isCancelled(): Boolean {
+        return cancel
+    }
+
+    override fun setCancelled(p0: Boolean) {
+        cancel = p0
+    }
+
+    companion object {
+        private val HANDLERS = HandlerList()
+
+        @JvmStatic
+        fun getHandlerList() : HandlerList {
+            return HANDLERS
+        }
+    }
+}
