62295f23e3acf09ab8ed0fa6a27b7002f32a6783
erudo <hashierudo@gmail.com>
2021-02-02 16:06:13 +0900

    [update] add 'admininfo' command
    'admininfo'コマンドの追加

--- src/main/kotlin/click/erudosaba/mc/eminejobs2/command/commands/subcommands/AdminInfo.kt
+++ src/main/kotlin/click/erudosaba/mc/eminejobs2/command/commands/subcommands/AdminInfo.kt
@@ -2,23 +2,45 @@ package click.erudosaba.mc.eminejobs2.command.commands.subcommands
 
 import click.erudosaba.mc.eminejobs2.Main
 import click.erudosaba.mc.eminejobs2.command.commands.SubCommand
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
 import org.bukkit.entity.Player
+import org.jetbrains.annotations.Nullable
 
 class AdminInfo(plugin: Main) : SubCommand() {
     override fun onCommand(player: Player, args: Array<String>) {
-        TODO("Not yet implemented")
+        if (args.isEmpty()) {
+            player.sendMessage("${ChatColor.BOLD}引数を指定してください")
+            return
+        }
+
+        val target = (if (Bukkit.getPlayer(args[0]) != null) Bukkit.getPlayer(args[0]) else return) ?: return
+
+        if (!Main.sqlUtil.isExists(target)) {
+            player.sendMessage("${target.name} は職業に就いていません")
+            return
+        } else {
+            val messages = arrayOf(
+                    "プレイヤー名： ${target.name}",
+                    "職業： ${Main.sqlUtil.getJob(target)}",
+                    "レベル； ${Main.sqlUtil.getLevel(target)}",
+                    "経験値： ${Main.sqlUtil.getExp(target)}"
+            )
+
+            messages.forEach { s -> player.sendMessage(s) }
+        }
     }
 
     override fun name(): String {
-        TODO("Not yet implemented")
+        return "admininfo"
     }
 
     override fun info(): String {
-        TODO("Not yet implemented")
+        return ""
     }
 
     override fun aliases(): Array<String> {
-        TODO("Not yet implemented")
+        return arrayOf(String())
     }
 
 }
