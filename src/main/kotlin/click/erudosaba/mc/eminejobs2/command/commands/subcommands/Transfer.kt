4a52dccda7db120a8c068c3a64f6db3a9466f829
erudo <hashierudo@gmail.com>
2021-02-02 16:37:35 +0900

    [update] add 'transfer' command
    'transfer'コマンドの追加

--- src/main/kotlin/click/erudosaba/mc/eminejobs2/command/commands/subcommands/Transfer.kt
+++ src/main/kotlin/click/erudosaba/mc/eminejobs2/command/commands/subcommands/Transfer.kt
@@ -2,23 +2,38 @@ package click.erudosaba.mc.eminejobs2.command.commands.subcommands
 
 import click.erudosaba.mc.eminejobs2.Main
 import click.erudosaba.mc.eminejobs2.command.commands.SubCommand
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
 import org.bukkit.entity.Player
 
 class Transfer(plugin: Main) : SubCommand() {
     override fun onCommand(player: Player, args: Array<String>) {
-        TODO("Not yet implemented")
+        if(args.size < 2) {
+            player.sendMessage("${ChatColor.BOLD}引数が足りません")
+            return
+        }
+
+        val target = (if (Bukkit.getPlayer(args[0]) != null) Bukkit.getPlayer(args[0]) else return) ?: return
+        val newjob = args[1]
+
+        if(Main.sqlUtil.isExists(target)) {
+            Main.sqlUtil.setJob(player,newjob);
+            player.sendMessage("${target.name}を${newjob}に就かせました")
+        } else {
+            player.sendMessage("${target.name}は無職です")
+        }
     }
 
     override fun name(): String {
-        TODO("Not yet implemented")
+        return "transfer"
     }
 
     override fun info(): String {
-        TODO("Not yet implemented")
+        return ""
     }
 
     override fun aliases(): Array<String> {
-        TODO("Not yet implemented")
+        return arrayOf(String())
     }
 
 }
