9cdde8ddc15d7d05ac3459711cc2c946db9bbcdc
erudo <hashierudo@gmail.com>
2021-02-02 16:32:08 +0900

    [update] add 'removeexp' command
    'removeexp'コマンドの追加

--- src/main/kotlin/click/erudosaba/mc/eminejobs2/command/commands/subcommands/RemoveExp.kt
+++ src/main/kotlin/click/erudosaba/mc/eminejobs2/command/commands/subcommands/RemoveExp.kt
@@ -2,23 +2,36 @@ package click.erudosaba.mc.eminejobs2.command.commands.subcommands
 
 import click.erudosaba.mc.eminejobs2.Main
 import click.erudosaba.mc.eminejobs2.command.commands.SubCommand
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
 import org.bukkit.entity.Player
 
 class RemoveExp(plugin: Main) : SubCommand() {
     override fun onCommand(player: Player, args: Array<String>) {
-        TODO("Not yet implemented")
+        if(args.size < 2) {
+            player.sendMessage("${ChatColor.BOLD}引数が足りません")
+            return
+        }
+
+        val target = (if (Bukkit.getPlayer(args[0]) != null) Bukkit.getPlayer(args[0]) else return) ?: return
+        val exp = args[1].toDoubleOrNull() ?: return
+
+        if(Main.sqlUtil.isExists(target)) {
+            Main.sqlUtil.setExp(target,Main.sqlUtil.getLevel(target) - exp)
+            player.sendMessage("${target.name}のレベルを${exp}さげました")
+        }
     }
 
     override fun name(): String {
-        TODO("Not yet implemented")
+        return "removeexp"
     }
 
     override fun info(): String {
-        TODO("Not yet implemented")
+        return ""
     }
 
     override fun aliases(): Array<String> {
-        TODO("Not yet implemented")
+        return arrayOf(String())
     }
 
 }
