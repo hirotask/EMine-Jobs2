28ed65309d3499576e53f8cc305288ae7f70427c
erudo <hashierudo@gmail.com>
2021-02-02 15:31:03 +0900

    [update] add 'stats' command
    'stats'コマンドの追加

--- src/main/kotlin/click/erudosaba/mc/eminejobs2/command/commands/subcommands/Stats.kt
+++ src/main/kotlin/click/erudosaba/mc/eminejobs2/command/commands/subcommands/Stats.kt
@@ -2,23 +2,36 @@ package click.erudosaba.mc.eminejobs2.command.commands.subcommands
 
 import click.erudosaba.mc.eminejobs2.Main
 import click.erudosaba.mc.eminejobs2.command.commands.SubCommand
+import org.bukkit.ChatColor
 import org.bukkit.entity.Player
 
 class Stats(plugin: Main) : SubCommand() {
     override fun onCommand(player: Player, args: Array<String>) {
-        TODO("Not yet implemented")
+        if(!Main.sqlUtil.isExists(player)) {
+            player.sendMessage("${ChatColor.BOLD}あなたは職業についていません")
+            return
+        }
+
+        val messages = arrayOf(
+                "=========================",
+                "職業： ${Main.sqlUtil.getJob(player)}",
+                "レベル： ${Main.sqlUtil.getLevel(player)}",
+                "現在の経験値： ${Main.sqlUtil.getExp(player)}"
+        )
+
+        messages.forEach { s -> player.sendMessage(s)}
     }
 
     override fun name(): String {
-        TODO("Not yet implemented")
+        return "stats"
     }
 
     override fun info(): String {
-        TODO("Not yet implemented")
+        return ""
     }
 
     override fun aliases(): Array<String> {
-        TODO("Not yet implemented")
+        return arrayOf(String())
     }
 
 }
