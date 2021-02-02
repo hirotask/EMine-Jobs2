c58dbd40089cc2f7ca221463308168b7f9d613a7
erudo <hashierudo@gmail.com>
2021-02-02 16:15:11 +0900

    [update] add 'employ' command
    'employ'コマンドの追加

--- src/main/kotlin/click/erudosaba/mc/eminejobs2/command/commands/subcommands/Employ.kt
+++ src/main/kotlin/click/erudosaba/mc/eminejobs2/command/commands/subcommands/Employ.kt
@@ -2,23 +2,38 @@ package click.erudosaba.mc.eminejobs2.command.commands.subcommands
 
 import click.erudosaba.mc.eminejobs2.Main
 import click.erudosaba.mc.eminejobs2.command.commands.SubCommand
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
 import org.bukkit.entity.Player
 
 class Employ(plugin: Main) : SubCommand() {
     override fun onCommand(player: Player, args: Array<String>) {
-        TODO("Not yet implemented")
+        //レベルを強制的にリセットし，転職させる
+
+        if(args.size < 2) {
+            player.sendMessage("${ChatColor.BOLD}引数が足りません")
+            return
+        }
+
+        val target = (if (Bukkit.getPlayer(args[0]) != null) Bukkit.getPlayer(args[0]) else return) ?: return
+        val job = args[1]
+
+        if(Main.sqlUtil.isExists(target)) {
+            Main.sqlUtil.delete(target)
+        }
+        Main.sqlUtil.insert(target,job)
     }
 
     override fun name(): String {
-        TODO("Not yet implemented")
+        return "employ"
     }
 
     override fun info(): String {
-        TODO("Not yet implemented")
+        return ""
     }
 
     override fun aliases(): Array<String> {
-        TODO("Not yet implemented")
+        return arrayOf(String())
     }
 
 }
