8d6ac604ff943c0aad203524b5ad2c08cb98a656
erudo <hashierudo@gmail.com>
2021-02-02 16:23:30 +0900

    [update] add 'demote' command
    'demote'コマンドの追加

--- src/main/kotlin/click/erudosaba/mc/eminejobs2/command/commands/subcommands/Demote.kt
+++ src/main/kotlin/click/erudosaba/mc/eminejobs2/command/commands/subcommands/Demote.kt
@@ -2,23 +2,36 @@ package click.erudosaba.mc.eminejobs2.command.commands.subcommands
 
 import click.erudosaba.mc.eminejobs2.Main
 import click.erudosaba.mc.eminejobs2.command.commands.SubCommand
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
 import org.bukkit.entity.Player
 
 class Demote(plugin: Main) : SubCommand() {
     override fun onCommand(player: Player, args: Array<String>) {
-        TODO("Not yet implemented")
+        if(args.size < 2) {
+            player.sendMessage("${ChatColor.BOLD}引数が足りません")
+            return
+        }
+
+        val target = (if (Bukkit.getPlayer(args[0]) != null) Bukkit.getPlayer(args[0]) else return) ?: return
+        val level = Integer.parseInt(args[1])
+
+        if(Main.sqlUtil.isExists(target)) {
+            Main.sqlUtil.setLevel(target,Main.sqlUtil.getLevel(target) - level)
+            player.sendMessage("${target.name}のレベルを${level}さげました")
+        }
     }
 
     override fun name(): String {
-        TODO("Not yet implemented")
+        return "demote"
     }
 
     override fun info(): String {
-        TODO("Not yet implemented")
+        return ""
     }
 
     override fun aliases(): Array<String> {
-        TODO("Not yet implemented")
+        return arrayOf(String())
     }
 
 }
